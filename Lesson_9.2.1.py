4cf3639ddc1e2e5c04fe601340e4b3aa6e0da283
MaxKifir4ik <162161173+Kifir4ikMax@users.noreply.github.com>
2024-03-31 14:21:13 +0300

    Create Lesson_9.2.1.py
    Version 1.0

---
+++ Lesson_9.2.1.py
@@ -0,0 +1,24 @@
+"""Задание 1
+
+Написать функцию check_login, которая будет принимать строку и проверять,
+что она является логином, который соответствует следующим правилам:
+1. Длина от 5 до 20 символов
+2. Состоит из букв верхнего и нижнего регистра, цифр, знаков подчеркивания
+
+
+"""
+
+#Импортируем из модуля регулярок под модуль findall
+from re import fullmatch
+def check_login():
+    while True:
+        login = input(str("Enter login: "))
+        if fullmatch(r"\w{5,20}", login):                   #Регулярка на валидность по символам и нижнему "_" ([a-zA-Z0-9_]). Можно дополнить кириллицей [a-zA-ZА-ЯЁа-яё0-9_]
+            print(f"Your login >>{login}<<")
+            break
+        else:
+            print("Invalid login, please enter again!")
+    return login
+    
+    
+check_login()
